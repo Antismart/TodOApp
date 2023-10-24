f1841af12161daecb7c732100998435f1188e4b0
Antismart <timbwamoses83@gmail.com>
2023-10-24 17:13:25 +0300

    Added MyTodoapp to MainActivity

--- app/src/main/java/com/example/basiccodelab/MainActivity.kt
+++ app/src/main/java/com/example/basiccodelab/MainActivity.kt
@@ -1,33 +1,39 @@
+
 package com.example.basiccodelab
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalComposeUiApi::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        val tasks = remember { mutableStateListOf<Task>() }
-
         setContent {
-            ToDoApp(
-                tasks = tasks,
-                onAddTask = { taskTitle ->
-                tasks.add(Task(tasks.size, taskTitle, false))
-            },
-                onTaskCheckedChange = { task, isChecked ->
-                    task.isCompleted = isChecked
-                }
-            )
+            MyToDoApp()
         }
     }
+}
+
+@Composable
+fun MyToDoApp() {
+    val tasks = remember { mutableStateListOf<Task>() }
+
+    ToDoApp(
+        tasks = tasks,
+        onAddTask = { taskTitle ->
+            tasks.add(Task(tasks.size, taskTitle, false))
+        },
+        onTaskCheckedChange = { task, isChecked ->
+            task.isCompleted = isChecked
+        }
+    )
 }
 
 //@Composable
